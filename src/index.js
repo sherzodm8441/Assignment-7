214f65e6a6fc4d8321d22546753cd2a9a9d3ffd4
bohuinong <bohuinong@yahoo.com>
2022-01-13 14:07:15 -0500

    Closed #2 SearchField Component that has state for the field input

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import SearchField from "./SearchField";
 
 
 ReactDOM.render(
   <React.StrictMode>
+    <SearchField />
     <App />
   </React.StrictMode>,
   document.getElementById('root')

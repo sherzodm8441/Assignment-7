104b55011bf7aa329100b1dcca8aae339ce72927
Sherzod Mirsharipov <mirsharipovsherzod@gmail.com>
2022-01-13 13:17:55 -0500

    closes #1; add App component: makes API calls for data

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+
+
 
 function App() {
+
+const apiKey = "aZFV2oHSbfWwZMoBp512mh5Z7ZY9I7Ni"
+
+const urlTrending = "http://api.giphy.com/v1/gifs/trending?api_key=" + apiKey
+const urlRandom = "http://api.giphy.com/v1/gifs/random?api_key=" + apiKey
+
+const [dataTrending, setDataTrending] = React.useState([])
+const [dataRandom, setDataRandom] = React.useState()
+const [called, setCalled] = React.useState(0)
+
+
+
+
+React.useEffect(()=>{
+  fetch(urlTrending)
+        .then(res => res.json())
+        .then(data => setDataTrending(data.data))
+        .catch(error  => console.log(error))
+},[])
+
+React.useEffect(()=>{
+  fetch(urlRandom)
+        .then(res => res.json())
+        .then(data => setDataRandom(data.data))
+        .catch(error  => console.log(error))
+  
+},[called])
+
+
+function changeCalled(){
+  setCalled(prevState => prevState + 1)
+}
+
+function returnRandom(){
+  return <img src={dataRandom["images"]["original"]["url"]} alt=""/>
+}
+const result = dataTrending.map(x => <img src={x["images"]["original"]["url"]} alt=""/>)
+//const randomGif = <img src={dataRandom["images"]["original"]["url"]} alt=""/> 
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={changeCalled}>Random Gif</button>
+      {called > 0 && returnRandom()}
+      <h1>Trending</h1>
+      {result}
+      
+      
     </div>
   );
 }
